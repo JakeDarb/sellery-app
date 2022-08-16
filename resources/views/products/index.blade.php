257d3b11703e5dc7f5cc887ee28e49e812fee395
Stijn Bouckaert <stijnbouckaert@Stijns-MacBook-Pro.local>
2022-08-16 14:53:31 +0200

    setup[products]: search

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts/app')
 
 @section('content')
-<h1>products</h1>
+    <h1>products</h1>
 
-@foreach($products as $product)
-<a href="/products/{{ $product->id }}">{{ $product->name }}</a>
-@endforeach
+    @livewire('product-search')
+
 @endsection
